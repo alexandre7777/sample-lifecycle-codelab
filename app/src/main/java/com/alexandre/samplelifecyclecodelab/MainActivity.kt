a0cb5bf1da9b397789d0a32d7141476265aa31e0
Alexandre <alexandre.genet7@gmail.com>
2018-04-29 21:52:49 +0200

    add ViewModel for chronometer orientation persistance changed

--- app/src/main/java/com/alexandre/samplelifecyclecodelab/MainActivity.kt
+++ app/src/main/java/com/alexandre/samplelifecyclecodelab/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.alexandre.samplelifecyclecodelab
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.os.SystemClock
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // The ViewModelStore provides a new ViewModel or one previously created.
+        val chronometerViewModel = ViewModelProviders.of(this).get(ChronometerViewModel::class.java)
+
+        if(chronometerViewModel.mStartTime == 0L)
+        {
+            val startTime = SystemClock.elapsedRealtime()
+            chronometerViewModel.mStartTime = startTime
+            chronometer.base = startTime
+        }
+        else
+        {
+            chronometer.base = chronometerViewModel.mStartTime
+        }
 
         chronometer.start()
     }
